f14c3e28e7d372f72a3622ccfbbb727e562e1b3a
Dims <smriti.ranjan152@gmail.com>
2026-09-21 14:19:19 +0530

    Add Valid Anagram solution

---
+++ Strings/ValidAnagram.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters in s
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Remove characters using t
+        for (char ch : t.toCharArray()) {
+
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if (map.get(ch) == 0) {
+                map.remove(ch);
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
