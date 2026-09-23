2f755de501ea1de5e3247e638477f05c7d201478
Dims <smriti.ranjan152@gmail.com>
2026-09-23 15:58:55 +0530

    Add Find Duplicate Number solution

---
+++ Arrays/FindDuplicateNumber.java
@@ -0,0 +1,24 @@
+public class FindDuplicateNumber {
+
+    public static int findDuplicate(int[] nums) {
+
+        // Phase 1: Detect cycle
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find cycle entrance
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
